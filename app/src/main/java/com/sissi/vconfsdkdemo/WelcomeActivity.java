18dd63571392cd3bef111ca4bcb9f88fca1a03fe
sissi <xingbiyanshu@gmail.com>
2018-10-13 21:28:24 +0800

    优化

--- app/src/main/java/com/sissi/vconfsdkdemo/WelcomeActivity.java
+++ app/src/main/java/com/sissi/vconfsdkdemo/WelcomeActivity.java
@@ -3,6 +3,7 @@ package com.sissi.vconfsdkdemo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.sissi.vconfsdk.base.AgentManager;
 import com.sissi.vconfsdk.startup.StartManager;
@@ -15,13 +16,6 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
-        StartManager startManager = AgentManager.obtain(StartManager.class);
-        startManager.startup(0, (i, o)->{
-//            KLog.p("#### resultCode=%s, response=%s ", i, o);
-            startActivity(new Intent(this, LoginActivity.class));
-//            printInfo();
-        });
     }
 
     @Override
@@ -33,4 +27,12 @@ public class WelcomeActivity extends AppCompatActivity {
         KLog.p("test");
     }
 
+    public void enter(View view) {
+        StartManager startManager = AgentManager.obtain(StartManager.class);
+        startManager.startup(0, (i, o)->{
+//            KLog.p("#### resultCode=%s, response=%s ", i, o);
+            startActivity(new Intent(this, LoginActivity.class));
+//            printInfo();
+        });
+    }
 }
