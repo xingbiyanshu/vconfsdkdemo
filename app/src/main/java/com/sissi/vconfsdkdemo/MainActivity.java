92821464ecc209592d05202d160ad73d3a83b1e6
xingbiyanshu <xingbiyanshu@gmail.com>
2018-10-23 11:28:09 +0800

    增加数据协作activity

--- app/src/main/java/com/sissi/vconfsdkdemo/MainActivity.java
+++ app/src/main/java/com/sissi/vconfsdkdemo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.sissi.vconfsdkdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.sissi.vconfsdk.base.AgentManager;
+import com.sissi.vconfsdk.base.MsgConst;
+import com.sissi.vconfsdk.base.ResultCode;
+import com.sissi.vconfsdk.datacollaborate.DataCollaborateManager;
 import com.sissi.vconfsdk.utils.KLog;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enter(View view) {
+        DataCollaborateManager dataCollaborateManager = AgentManager.obtain(DataCollaborateManager.class);
+        dataCollaborateManager.login("127.0.0.1", 6666, MsgConst.EmDcsType.emTypeTrueTouchPhoneAndroid, (i, o)->{
+            KLog.p("#### resultCode=%s, response=%s ", i, o);
+            if (ResultCode.SUCCESS == i){
+                startActivity(new Intent(this, DataCollaborateActivity.class));
+            }
+        });
     }
 
 //    @Override
