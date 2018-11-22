057bbe67c99e86a4d050f58874fdf42ef7f1cde4
xingbiyanshu <xingbiyanshu@gmail.com>
2018-11-22 22:08:38 +0800

    适配

--- app/src/main/java/com/sissi/vconfsdkdemo/MainActivity.java
+++ app/src/main/java/com/sissi/vconfsdkdemo/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import com.kedacom.vconf.sdk.base.AgentManager;
+import com.kedacom.vconf.sdk.base.KLog;
 import com.kedacom.vconf.sdk.base.MsgConst;
 import com.kedacom.vconf.sdk.base.CommonResultCode;
 import com.kedacom.vconf.sdk.datacollaborate.DataCollaborateManager;
@@ -23,14 +24,14 @@ public class MainActivity extends AppCompatActivity {
     public void enter(View view) {
         DataCollaborateManager dataCollaborateManager = AgentManager.obtain(DataCollaborateManager.class);
         dataCollaborateManager.login("127.0.0.1", 6666, TerminalType.TrueLinkAndroidPhone, (i, o)->{
-//            KLog.p("#### CommonResultCode=%s, response=%s ", i, o);
-//            if (CommonResultCode.SUCCESS == i){
-//                dataCollaborateManager.createDcConf((rc, r)->{
-//                    if (CommonResultCode.SUCCESS == rc) startActivity(new Intent(this, DataCollaborateActivity.class));
-//                });
-//            }
-
-            startActivity(new Intent(this, DataCollaborateActivity.class));
+            KLog.p("#### CommonResultCode=%s, response=%s ", i, o);
+            if (CommonResultCode.SUCCESS == i){
+                dataCollaborateManager.createDcConf((rc, r)->{
+                    if (CommonResultCode.SUCCESS == rc) startActivity(new Intent(this, DataCollaborateActivity.class));
+                });
+            }
+//
+//            startActivity(new Intent(this, DataCollaborateActivity.class));
         });
     }
 
