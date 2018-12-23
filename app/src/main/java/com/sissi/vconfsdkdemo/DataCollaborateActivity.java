b3284b485e4d77660cc99be5723e01d1cd878064
sissi <xingbiyanshu@gmail.com>
2018-12-23 21:26:02 +0800

    适配

--- app/src/main/java/com/sissi/vconfsdkdemo/DataCollaborateActivity.java
+++ app/src/main/java/com/sissi/vconfsdkdemo/DataCollaborateActivity.java
@@ -1,16 +1,12 @@
 package com.sissi.vconfsdkdemo;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.kedacom.vconf.sdk.base.KLog;
 import com.kedacom.vconf.sdk.base.Msg;
 import com.kedacom.vconf.sdk.datacollaborate.DataCollaborateManager;
 import com.kedacom.vconf.sdk.datacollaborate.DefaultPaintFactory;
-import com.kedacom.vconf.sdk.datacollaborate.DefaultPainter;
 import com.kedacom.vconf.sdk.datacollaborate.IPaintBoard;
 import com.kedacom.vconf.sdk.datacollaborate.IPaintFactory;
 import com.kedacom.vconf.sdk.datacollaborate.IPainter;
@@ -19,12 +15,9 @@ import com.kedacom.vconf.sdk.datacollaborate.bean.OpPaint;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class DataCollaborateActivity extends AppCompatActivity
         implements DataCollaborateManager.IOnBoardOpListener,
@@ -47,43 +40,14 @@ public class DataCollaborateActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-
         boardContainer = findViewById(R.id.data_collaborate_content);
 
-        KLog.p("dataCollaborateManager.setPainter");
         dataCollaborateManager = DataCollaborateManager.getInstance(this.getApplication());
         dataCollaborateManager.addBoardOpListener(this);
         dataCollaborateManager.addPaintOpListener(this);
-//        RawPainter painter = new RawPainter(this);
-//        DefaultPainter painter = new DefaultPainter(this);
-
-//        dataCollaborateManager.setPainter(painter);
-//        ViewGroup vg = findViewById(R.id.data_collaborate_content);
-////        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-//        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-
-//        vg.addView(painter, params);
-
-//        View v = painter.getPaintView();
-//        v.setAlpha(0.2f);
-//        v.setBackgroundColor(Color.BLUE);
-//        vg.addView(v, params);
-
-//        vg.setBackgroundColor(Color.BLUE);
-
-//        new Handler().postDelayed(dataCollaborateManager::ejectNtfs, 1000);
 
         paintFactory = new DefaultPaintFactory(this);
         painter = paintFactory.createPainter();
-
 
     }
 
@@ -206,30 +170,30 @@ public class DataCollaborateActivity extends AppCompatActivity
 
     @Override
     public void onPaintOp(OpPaint opPaint) {
-        KLog.p("UI onPaintOp:%s", opPaint);
+//        KLog.p("UI onPaintOp:%s", opPaint);
         painter.paint(opPaint);
     }
 
     @Override
     public void publish(OpPaint op) {
-        KLog.p(KLog.WARN,"publish paint op %s", op);
+//        KLog.p(KLog.WARN,"publish paint op %s", op);
         dataCollaborateManager.publishPaintOp(op);
     }
 
 
     @Override
     public void onZoomRateChanged(int percentage) {
-        KLog.p(KLog.WARN,"rate= %s", percentage);
+//        KLog.p(KLog.WARN,"rate= %s", percentage);
     }
 
     @Override
     public void onPictureCountChanged(int count) {
-        KLog.p(KLog.WARN,"count= %s", count);
+//        KLog.p(KLog.WARN,"count= %s", count);
     }
 
     @Override
     public void onRepealableStateChanged(int repealedOpsCount, int remnantOpsCount) {
-        KLog.p(KLog.WARN,"repealedOpsCount=%s, remnantOpsCount=%s", repealedOpsCount, remnantOpsCount);
+//        KLog.p(KLog.WARN,"repealedOpsCount=%s, remnantOpsCount=%s", repealedOpsCount, remnantOpsCount);
     }
 
 }
