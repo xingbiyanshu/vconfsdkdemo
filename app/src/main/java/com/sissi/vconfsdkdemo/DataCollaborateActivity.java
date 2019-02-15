10d59b537bd230168fccb1201b365ba7e3bf1404
xingbiyanshu <xingbiyanshu@gmail.com>
2019-02-15 15:09:58 +0800

    适配

--- app/src/main/java/com/sissi/vconfsdkdemo/DataCollaborateActivity.java
+++ app/src/main/java/com/sissi/vconfsdkdemo/DataCollaborateActivity.java
@@ -3,9 +3,13 @@ package com.sissi.vconfsdkdemo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.graphics.RectF;
 import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.kedacom.vconf.sdk.base.IResultListener;
 import com.kedacom.vconf.sdk.base.KLog;
 import com.kedacom.vconf.sdk.base.Msg;
 import com.kedacom.vconf.sdk.datacollaborate.DataCollaborateManager;
@@ -15,19 +19,31 @@ import com.kedacom.vconf.sdk.datacollaborate.IPaintBoard;
 import com.kedacom.vconf.sdk.datacollaborate.IPaintFactory;
 import com.kedacom.vconf.sdk.datacollaborate.IPainter;
 import com.kedacom.vconf.sdk.datacollaborate.bean.BoardInfo;
+import com.kedacom.vconf.sdk.datacollaborate.bean.OpClearScreen;
 import com.kedacom.vconf.sdk.datacollaborate.bean.OpPaint;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.os.Environment;
 import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DataCollaborateActivity extends AppCompatActivity
-        implements DataCollaborateManager.IOnBoardOpListener, DataCollaborateManager.IOnPaintOpListener, IPaintBoard.IPublisher {
+        implements DataCollaborateManager.IOnBoardOpListener, DataCollaborateManager.IOnPaintOpListener, IPaintBoard.IPublisher/*, IPaintBoard.IOnRepealableStateChangedListener, IPaintBoard.IOnZoomRateChangedListener, IPaintBoard.IOnPictureCountChanged*/ {
 
     DataCollaborateManager dataCollaborateManager;
     IPaintFactory paintFactory;
@@ -42,68 +58,177 @@ public class DataCollaborateActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         boardContainer = findViewById(R.id.data_collaborate_content);
 
         KLog.p("dataCollaborateManager.setPainter");
-        dataCollaborateManager = DataCollaborateManager.getInstance(this);
+        dataCollaborateManager = DataCollaborateManager.getInstance(this.getApplication());
         dataCollaborateManager.addBoardOpListener(this);
         dataCollaborateManager.addPaintOpListener(this);
-//        RawPainter painter = new RawPainter(this);
-//        DefaultPainter painter = new DefaultPainter(this);
-
-//        dataCollaborateManager.setPainter(painter);
-//        ViewGroup vg = findViewById(R.id.data_collaborate_content);
-////        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-//        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-
-//        vg.addView(painter, params);
-
-//        View v = painter.getPaintView();
-//        v.setAlpha(0.2f);
-//        v.setBackgroundColor(Color.BLUE);
-//        vg.addView(v, params);
-
-//        vg.setBackgroundColor(Color.BLUE);
-
-//        new Handler().postDelayed(dataCollaborateManager::ejectNtfs, 1000);
 
         paintFactory = new DefaultPaintFactory(this);
         painter = paintFactory.createPainter();
 
+        // DEBUG
+//        RectF rect1 = new RectF(10, 10, 20, 30);
+//        RectF rect2 = new RectF(10, 10, 20, 30);
+//        RectF rect3 = new RectF(10, 10, 40, 70);
+//        Matrix transMatrix1 = calcTransMatrix(rect1, rect2);
+//        Matrix transMatrix2 = calcTransMatrix(rect1, rect3);
+//        RectF rect12 = new RectF();
+//        RectF rect13 = new RectF();
+//        transMatrix1.mapRect(rect12, rect1);
+//        transMatrix2.mapRect(rect13, rect1);
+//        KLog.p("transMatrix1=%s, transMatrix2=%s, \nrect1=%s, rect2=%s, rect3=%s, rect12=%s, rect13=%s",
+//                transMatrix1, transMatrix2, rect1, rect2, rect3, rect12, rect13);
+//
+//        PointF pointF = new PointF(448, 0); // 100%时的插入点
+//        float[] mappedPoint = new float[]{pointF.x, pointF.y};
+//        Matrix matrix1 = new Matrix();
+//        Matrix matrix2 = new Matrix();
+//        Matrix invertedMatrix1 = new Matrix();
+//        Matrix invertedMatrix2 = new Matrix();
+//        matrix1.postTranslate(10, 10);
+//        matrix2.postTranslate(30, 30);
+//        matrix1.mapRect(rect1);
+//        matrix2.mapRect(rect2);
+//        matrix1.invert(invertedMatrix1);
+//        matrix2.postConcat(invertedMatrix1);
+//        invertedMatrix1.mapRect(rect3);
+//        KLog.p("matrix1=%s, matrix2=%s, invertedMatrix1=%s, rect1=%s, rect2=%s, rect3=%s",
+//                matrix1, matrix2, invertedMatrix1, rect1, rect2, rect3);
+
+//
+//        matrix1.postScale(0.5f, 0.5f, 960, 540); // 全屏缩放为50%后的matrix
+//        matrix1.invert(invertedMatrix1);
+//        invertedMatrix1.invert(invertedMatrix2);
+//        invertedMatrix1.mapPoints(mappedPoint);  // 适应全屏缩放50%后的插入点，使得实际插入的位置跟见到位置一致。
+//        float[] vals = new float[]{0.6666667f, 0.0f, 161.33334f, 0.0f, 0.6666667f, 223.54288f, 0.0f, 0.0f, 1.0f};
+//        matrix1.setValues(vals);
+//        float[] x = new float[]{0, 0};
+//        matrix1.mapPoints(x);
+//        KLog.p("matrix1=%s, mappedPoints=[%s, %s]", matrix1, x[0], x[1]);
+//        matrix1.mapPoints(mappedPoint);
+//        KLog.p("after 0.5scaled matrix=%s, invertedMatrix1=%s, invert mappedPoints=[%s, %s]", matrix1, invertedMatrix1, mappedPoint[0], mappedPoint[1]);
+//        matrix2.postTranslate(40, 40);
+//        matrix2.postScale(2, 2);
+//        matrix1.mapPoints(mappedPoint);
+//        Matrix matrix2DivMatrix1 = new Matrix(matrix2);
+//        matrix2DivMatrix1.postConcat(invertedMatrix1);
+//        KLog.p("matrix1=%s, matrix2=%s, matrix2DivMatrix1=%s, \npoint=%s, matrix1MappedPoint=[%s,%s]\ninvertedMatrix1=%s, invertedMatrix2=%s",
+//                matrix1, matrix2, matrix2DivMatrix1, pointF, mappedPoint[0], mappedPoint[1], invertedMatrix1, invertedMatrix2);
+//        Matrix matrix1ConcatMatrix2 = new Matrix(matrix1);
+//        matrix1ConcatMatrix2.postConcat(matrix2);
+//        Matrix matrix2ConcatMatrix1 = new Matrix(matrix2);
+//        matrix2ConcatMatrix1.postConcat(matrix1);
+//        KLog.p("matrix1=%s, matrix2=%s, \nmatrix1ConcatMatrix2=%s, matrix2ConcatMatrix1=%s", matrix1, matrix2, matrix1ConcatMatrix2, matrix2ConcatMatrix1);
+//
+//        Matrix matrix1XMatrix2XInvertedMatrix2 = new Matrix(matrix1ConcatMatrix2);
+//        matrix1XMatrix2XInvertedMatrix2.postConcat(invertedMatrix2);
+//        Matrix matrix1XMatrix2XInvertedMatrix1 = new Matrix(matrix1ConcatMatrix2);
+//        matrix1XMatrix2XInvertedMatrix1.postConcat(invertedMatrix1);
+//        KLog.p("matrix1XMatrix2XInvertedMatrix2=%s, matrix1XMatrix2XInvertedMatrix1=%s", matrix1XMatrix2XInvertedMatrix2, matrix1XMatrix2XInvertedMatrix1);
 
     }
 
 
     static Bitmap bt;
+    static String filePath;
     public void download(View view) {
 //        bt = BitmapFactory.decodeFile("/data/local/tmp/wb.png");
-        if (null == bt) {
-            painter.getCurrentPaintBoard().getBoardView().setBackgroundColor(Color.BLUE);
-            bt = painter.getCurrentPaintBoard().snapshot(IPaintBoard.LAYER_ALL);
-        }else {
-            bt = painter.getCurrentPaintBoard().snapshot(IPaintBoard.LAYER_PIC_AND_SHAPE);
-        }
+//        if (null == bt) {
+//            painter.getCurrentPaintBoard().getBoardView().setBackgroundColor(Color.BLUE);
+//            bt = painter.getCurrentPaintBoard().snapshot(IPaintBoard.LAYER_ALL);
+//        }else {
+//            bt = painter.getCurrentPaintBoard().snapshot(IPaintBoard.LAYER_PIC_AND_SHAPE);
+//        }
+        painter.getCurrentPaintBoard().save(new IPaintBoard.ISaveListener() {
+            @Override
+            public void onFinish(Bitmap bitmap) {
+                bt = bitmap;
+                File file = new File(Environment.getExternalStorageDirectory(), "snapshot.png");
+                filePath = file.getPath();
+                KLog.p("filePath=%s", filePath);
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(file);
+                    //通过io流的方式来压缩保存图片
+                    boolean isSuccess = bt.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                    fos.flush();
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
     }
 
     public void onCreatePaintBoardClicked(View view) {
-        dataCollaborateManager.ejectNtf(Msg.DCBoardCreatedNtf);
-    }
+        dataCollaborateManager.newBoard("mye164", new IResultListener() {
+            @Override
+            public void onSuccess(Object result) {
+                KLog.p("newBoard success");
+                createBoard((BoardInfo) result);
+            }
 
-    public void onSwitchPaintBoardClicked(View view) {
-        dataCollaborateManager.ejectNtf(Msg.DCBoardSwitchedNtf);
+            @Override
+            public void onFailed(int errorCode) {
+                KLog.p("newBoard fail");
+            }
+
+            @Override
+            public void onTimeout() {
+                KLog.p("newBoard timeout");
+            }
+        });
     }
 
     public void onDeletePaintBoardClicked(View view) {
-        dataCollaborateManager.ejectNtf(Msg.DCBoardDeletedNtf);
+        dataCollaborateManager.delBoard("board", new IResultListener() {
+            @Override
+            public void onSuccess(Object result) {
+                KLog.p("del board success");
+                delBoard((String) result);
+            }
+
+            @Override
+            public void onFailed(int errorCode) {
+                KLog.p("del board success");
+
+            }
+
+            @Override
+            public void onTimeout() {
+                KLog.p("del board success");
+
+            }
+        });
+    }
+
+
+    public void onSwitchPaintBoardClicked(View view) {
+        dataCollaborateManager.switchBoard("board", new IResultListener() {
+            @Override
+            public void onSuccess(Object result) {
+                KLog.p("switch board success");
+                switchBoard((String) result);
+            }
+
+            @Override
+            public void onFailed(int errorCode) {
+                KLog.p("switch board failed");
+            }
+
+            @Override
+            public void onTimeout() {
+                KLog.p("switch board timeout");
+            }
+        });
     }
 
     public void onEraseClicked(View view) {
@@ -123,7 +248,10 @@ public class DataCollaborateActivity extends AppCompatActivity
 
     public void onClearScreenClicked(View view) {
 //        dataCollaborateManager.ejectNtf(Msg.DCScreenClearedNtf);
-        painter.getCurrentPaintBoard().clearScreen();
+//        painter.getCurrentPaintBoard().clearScreen();
+        OpClearScreen opClearScreen = new OpClearScreen();
+        opClearScreen.setBoardId("boardId");
+//        painter.paint(opClearScreen);
     }
 
     public void onDrawLineClicked(View view) {
@@ -149,15 +277,17 @@ public class DataCollaborateActivity extends AppCompatActivity
     public void onInsertPicClicked(View view) {
 //        dataCollaborateManager.ejectNtf(Msg.DCPicInsertedNtf);
 //        bt = BitmapFactory.decodeFile("/data/local/tmp/wb.png");
-        painter.getCurrentPaintBoard().insertPic(bt);
+//        painter.getCurrentPaintBoard().insertPic("/data/local/tmp/Penguins.jpg");
+        KLog.p("filePath=%s", filePath);
+        painter.getCurrentPaintBoard().insertPic(filePath);
     }
 
     public void onDeletePicClicked(View view) {
-        dataCollaborateManager.ejectNtf(Msg.DCPicDeletedNtf);
+//        dataCollaborateManager.ejectNtf(Msg.DCPicDeletedNtf);
     }
 
     public void onMovePic(View view) {
-        dataCollaborateManager.ejectNtf(Msg.DCPicDraggedNtf);
+//        dataCollaborateManager.ejectNtf(Msg.DCPicDraggedNtf);
     }
 
     public void onUndoClicked(View view) {
@@ -170,42 +300,82 @@ public class DataCollaborateActivity extends AppCompatActivity
         painter.getCurrentPaintBoard().redo();
     }
 
+    private void createBoard(BoardInfo boardInfo){
+        IPaintBoard paintBoard = paintFactory.createPaintBoard(boardInfo);
+        paintBoard.setPublisher(DataCollaborateActivity.this);
+        painter.addPaintBoard(paintBoard); // 创建画板并添加给painter管理。
+    }
+
+    private void switchBoard(String boardId){
+        IPaintBoard curPaintBoard = painter.getCurrentPaintBoard();
+        // painter切换到目标画板
+        IPaintBoard switchToPaintBoard = painter.switchPaintBoard(boardId);
+        if (null != switchToPaintBoard){
+            // view 树中删除当前画板 NOTE:这个取决于界面的具体需求，此例界面仅展示一张画板
+            if (null != curPaintBoard) {
+                boardContainer.removeView(curPaintBoard.getBoardView());
+            }
+            // view树中添加目标画板
+            boardContainer.addView(switchToPaintBoard.getBoardView(),
+                    new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        }
+    }
+
+    private void delBoard(String boardId){
+        // 从painter删除该画板
+        IPaintBoard paintBoard = painter.deletePaintBoard(boardId);
+        if (null != paintBoard){
+            // 从界面View树中移除该画板
+            boardContainer.removeView(paintBoard.getBoardView());
+        }
+    }
+
     @Override
     public void onBoardCreated(BoardInfo boardInfo) {
-        IPaintBoard paintBoard = paintFactory.createPaintBoard(boardInfo);
-        paintBoard.setPublisher(this);
-        painter.addPaintBoard(paintBoard);
-        painter.setRole(boardInfo.getId(), IPainter.ROLE_AUTHOR);
+        KLog.p("onBoardCreated");
+        createBoard(boardInfo);
     }
 
     @Override
     public void onBoardDeleted(String boardId) {
-        IPaintBoard paintBoard = painter.getPaintBoard(boardId);
-        if (null != paintBoard){
-            boardContainer.removeView(paintBoard.getBoardView());
-            painter.deletePaintBoard(boardId);
-        }
+        delBoard(boardId);
     }
 
     @Override
     public void onBoardSwitched(String boardId) {
-        IPaintBoard curPaintBoard = painter.switchPaintBoard(boardId);
-        if (null != curPaintBoard){
-            View v = curPaintBoard.getBoardView();
-            boardContainer.addView(v,  new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        }
+        switchBoard(boardId);
     }
-
 
     @Override
-    public void onPaintOp(OpPaint opPaint) {
-        KLog.p("UI onPaintOp:%s", opPaint);
-        painter.paint(opPaint);
+    public void onAllBoardDeleted() {
+
     }
+
 
     @Override
     public void publish(OpPaint op) {
         KLog.p(KLog.WARN,"publish paint op %s", op);
-        dataCollaborateManager.publishPaintOp(op);
+//        dataCollaborateManager.publishPaintOp(op);
     }
+
+    @Override
+    public void onPaintOp(List<OpPaint> ops) {
+        painter.paint(ops);
+    }
+
+//
+//    @Override
+//    public void onZoomRateChanged(int percentage) {
+//
+//    }
+//
+//    @Override
+//    public void onPictureCountChanged(int count) {
+//
+//    }
+//
+//    @Override
+//    public void onRepealableStateChanged(int repealedOpsCount, int remnantOpsCount) {
+//
+//    }
 }
--- app/src/main/java/com/sissi/vconfsdkdemo/MainActivity.java
+++ app/src/main/java/com/sissi/vconfsdkdemo/MainActivity.java
@@ -9,57 +9,65 @@ import android.view.View;
 
 import com.kedacom.vconf.sdk.base.IResultListener;
 import com.kedacom.vconf.sdk.base.KLog;
-import com.kedacom.vconf.sdk.base.basement.EmulationModeOnOff;
 import com.kedacom.vconf.sdk.datacollaborate.DataCollaborateManager;
+import com.kedacom.vconf.sdk.datacollaborate.IPaintBoard;
+import com.kedacom.vconf.sdk.datacollaborate.IPaintFactory;
+import com.kedacom.vconf.sdk.datacollaborate.IPainter;
+import com.kedacom.vconf.sdk.datacollaborate.bean.BoardInfo;
+//import com.kedacom.vconf.sdk.datacollaborate.bean.CreateConfResult;
+import com.kedacom.vconf.sdk.datacollaborate.bean.DCMember;
+import com.kedacom.vconf.sdk.datacollaborate.bean.DcConfInfo;
+import com.kedacom.vconf.sdk.datacollaborate.bean.EConfType;
+import com.kedacom.vconf.sdk.datacollaborate.bean.EDcMode;
 import com.kedacom.vconf.sdk.datacollaborate.bean.ETerminalType;
+import com.kedacom.vconf.sdk.datacollaborate.bean.OpPaint;
+
+import java.util.List;
 //import com.kedacom.vconf.sdk.utils.KLog;
 
 public class MainActivity extends AppCompatActivity {
+    DataCollaborateManager dm;
+    DataCollaborateManager dataCollaborateManager;
+    IPaintFactory paintFactory;
+    IPainter painter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EmulationModeOnOff.enable(true);
     }
 
+
+
     public void enter(View view) {
-//        startActivity(new Intent(MainActivity.this, DataCollaborateActivity.class));
 
+        dataCollaborateManager = DataCollaborateManager.getInstance(this.getApplication());
 
-        DataCollaborateManager dataCollaborateManager = DataCollaborateManager.getInstance(this);
-        dataCollaborateManager.login("127.0.0.1", 6666, ETerminalType.TrueLinkAndroidPhone, new IResultListener() {
-//            @Override
-//            public LifecycleOwner getLifecycleOwner() {
-//                KLog.p("getLifecycleOwner");
-//                return MainActivity.this;
-//            }
+        dataCollaborateManager.enableSimulator(true);
 
-            @Override
-            public void onArrive() {
-                KLog.p("onResultArrived");
-                startActivity(new Intent(MainActivity.this, DataCollaborateActivity.class));
-            }
+        dataCollaborateManager.login("127.0.0.1", 6666, ETerminalType.TrueLinkAndroidPhone,
+                new IResultListener() {
 
             @Override
             public void onSuccess(Object result) {
                 KLog.p("onSuccess");
-//                dataCollaborateManager.createDcConf(new IResultListener() {
-//                    @Override
-//                    public void onSuccess(Object result) {
-//                        startActivity(new Intent(MainActivity.this, DataCollaborateActivity.class));
-//                    }
-//
-//                    @Override
-//                    public void onFailed(int errorCode) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onTimeout() {
-//
-//                    }
-//                });
+                dataCollaborateManager.createDcConf("confE164", "confName", EDcMode.Auto, EConfType.MCC, "adminE164", null,
+                        new IResultListener() {
+                    @Override
+                    public void onSuccess(Object result) {
+                        startActivity(new Intent(MainActivity.this, DataCollaborateActivity.class));
+                    }
+
+                    @Override
+                    public void onFailed(int errorCode) {
+
+                    }
+
+                    @Override
+                    public void onTimeout() {
+
+                    }
+                });
             }
 
             @Override
@@ -73,7 +81,10 @@ public class MainActivity extends AppCompatActivity {
 
                 KLog.p("onTimeout");
             }
-        });
+        }
+        );
+
+
     }
 
 //    @Override
